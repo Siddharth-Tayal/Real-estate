139a5d74ea7e5030a273b8f957b1e9c8b04b6df7
Siddharth Tayal <125152950+Siddharth-Tayal@users.noreply.github.com>
2023-10-25 16:22:59 +0530

    Update User Role
    Initially making all the user to admin to show all the functionality of Website.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        default: "user",
+        default: "admin",
     },
     avatar: {
         type: String,
